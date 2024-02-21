5a085df011c907c8587c1058c92349ef49a57d34
davidthomashess <ddtmhess@gmail.com>
2024-02-21 23:26:45 +0000

    Make content header display for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <main className="flex justify-center border-2 border-red-900 pt-0 md:pt-2">
       <div className="md:w-[768px] w-full bg-dark-grey-blue pt-2 md:pt-0">
-        <h1 className="text-black md:text-lg p-1  text-center text-3xl mb-2 md:mb-0">
+        <h1
+          className="text-black md:text-lg p-1 text-center text-3xl mb-2 md:mb-0"
+          data-testid="page-header"
+        >
           Notification Form
         </h1>
         <Form />
